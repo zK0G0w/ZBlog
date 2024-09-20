6da93ef52117af9a00dc7cb7ba144399b90f9685
Wain Zeng <zk0g0w@gmail.com>
2024-09-20 23:06:50 +0800

    Init

---
+++ blog-view/src/main/java/com/zkogow/ZkogowBlogApplication.java
@@ -0,0 +1,18 @@
+package com.zkogow;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: WainZeng
+ * @Date: 2024/9/20 22:48
+ */
+
+@SpringBootApplication
+public class ZkogowBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZkogowBlogApplication.class, args);
+    }
+}
